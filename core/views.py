d0c1affd63aa065db8c8eacf95c4cd693f07a48a
Bruno Alves <Bruno6821@gmail.com>
2021-10-29 21:54:43 -0300

    Configurando as ROTAS.

--- core/views.py
+++ core/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'core/index.html')
 
 
 def contato(request):
-    return render(request, 'contato.html')
+    return render(request, 'core/contato.html')
 
 
 def produto(request):
-    return render(request, 'produto.html')
+    return render(request, 'core/produto.html')
